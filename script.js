a03587b3cbf7d5e1208d5a95aa498ca97415618c
RafaelRamosEsposito <rafaelramosesposito@gmail.com>
2021-05-24 09:59:55 -0300

    Finalizei o quinto desafio sobre lógica

---
+++ script.js
@@ -0,0 +1,9 @@
+let moveis = ["mesa", "cadeira", "sofa", "armario", "cama"]
+moveis.unshift("mesinha")
+moveis.pop()
+moveis.push("estante","prateleire")
+moveis.shift()
+console.log (moveis)
+let constNumbers = [7,5,6,3,8,9,2,1,4]
+constNumbers.sort ()
+console.log (constNumbers)
